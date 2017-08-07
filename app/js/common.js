cb48f3da3ea09d910e436a0fb2a15d50136332d2
don1420 <don1420@ukr.net>
2017-08-07 17:17:42 +0300

    1920px

---
+++ app/js/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+	/*function heightDetect() {
+		var height1 = $(".roads").css("height");
+		var height2 = $(".our_guarantees").css("height");
+		$(".bg_1").css("height", parseInt(height1));
+		$(".bg_2").css("height", parseInt(height2));
+	};
+
+	heightDetect();
+
+	$(window).resize(function() {
+		heightDetect();
+	});*/
+
+
+});
